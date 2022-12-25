2ed84eed9dea2ea36a78fb0d25edc63fc2404e48
ThePedroo <pedroolimpio152@gmail.com>
2022-12-24 23:14:57 -0300

    [IMPROVED] Replaced pos keyword for hash keyword.
    To avoid confusion with the pos keyword, which is used to refer to the position of an element in the table, the hash keyword is also used for this, but being more specific.

--- tablec.c
+++ tablec.c
@@ -23,7 +23,7 @@ void tablec_init(struct hashtable *tablec, size_t max_capacity, int secure_mode)
 }
 
 void tablec_resize(struct hashtable *tablec, size_t new_max_capacity) {
-  /* TODO: Add rehashing when resizing hashtable because it will cause __tablec_mkIndex to not find the keys if not. */
+  /* TODO: Add rehashing when resizing hashtable because it will cause __tablec_hash to not find the keys if not. */
   tablec->capacity = new_max_capacity;
   tablec->buckets = realloc(tablec->buckets, sizeof(struct hashtable_buckets) * new_max_capacity);
 }
@@ -33,21 +33,21 @@ void __tablec_expandBucketArr(struct hashtable_buckets *bucket, size_t new_max_c
   bucket->array = realloc(bucket->array, sizeof(struct hashtable_buckets_array) * new_max_capacity);
 }
 
-size_t __tablec_mkIndex(struct hashtable *tablec, char *key, size_t keyLength) {
-  size_t pos = 0, i = 0;
+size_t __tablec_hash(struct hashtable *tablec, char *key, size_t keyLength) {
+  size_t hash = 0, i = 0;
 
   if (keyLength == 0) keyLength = strlen(key);
 
   do {
-    pos = pos * 37 + (key[i] & 255);
+    hash = hash * 37 + (key[i] & 255);
     i++;
   } while (i < keyLength);
 
-  return pos % tablec->capacity;
+  return hash % tablec->capacity;
 }
 
 void tablec_set(struct hashtable *tablec, char *key, size_t keyLength, void *value) {
-  size_t pos;
+  size_t hash;
   if (tablec->length >= tablec->capacity) {
     if (tablec->secure_mode) {
       tablec_resize(tablec, tablec->capacity * 2);
@@ -55,96 +55,73 @@ void tablec_set(struct hashtable *tablec, char *key, size_t keyLength, void *val
       printf("TableC error, the buckets are full. Use tablec_expand or secure mode to fix.\n");
     }
   }
-  pos = __tablec_mkIndex(tablec, key, keyLength);
+  hash = __tablec_hash(tablec, key, keyLength);
 
   tablec->length++;
 
-  if (tablec->buckets[pos].capacity == 1) {
-    if (!tablec->buckets[pos].array[0].filled) {
-      tablec->buckets[pos].array[0].key = key;
-      tablec->buckets[pos].array[0].value = value;
-      tablec->buckets[pos].array[0].filled = 1;
+  if (tablec->buckets[hash].capacity == 1) {
+    if (tablec->buckets[hash].array[0].key == NULL) {
+      tablec->buckets[hash].array[0].key = key;
+      tablec->buckets[hash].array[0].value = value;
 
-      tablec->buckets[pos].length = 1;
+      tablec->buckets[hash].length = 1;
       return;
     } else {
-      __tablec_expandBucketArr(&tablec->buckets[pos], 2);
+      __tablec_expandBucketArr(&tablec->buckets[hash], 2);
 
-      tablec->buckets[pos].array[1].key = key;
-      tablec->buckets[pos].array[1].value = value;
-      tablec->buckets[pos].array[1].filled = 1;
+      tablec->buckets[hash].array[1].key = key;
+      tablec->buckets[hash].array[1].value = value;
 
-      tablec->buckets[pos].length = 2;
+      tablec->buckets[hash].length = 2;
       return;
     }
   } else {
-    switch(tablec->buckets[pos].emptyCapacity) {
+    switch(tablec->buckets[hash].emptyCapacity) {
       case 0: {
         size_t i = -1;
 
         do {
           i++;
-          if (!tablec->buckets[pos].array[i].filled) {
-            tablec->buckets[pos].array[i].key = key;
-            tablec->buckets[pos].array[i].value = value;
-            tablec->buckets[pos].array[i].filled = 1;
+          if (tablec->buckets[hash].array[i].key == NULL) {
+            tablec->buckets[hash].array[i].key = key;
+            tablec->buckets[hash].array[i].value = value;
 
-            tablec->buckets[pos].length++;
+            tablec->buckets[hash].length++;
             break;
           }
 
-          if (i != tablec->buckets[pos].length - 1) continue;
+          if (i != tablec->buckets[hash].length - 1) continue;
 
-          __tablec_expandBucketArr(&tablec->buckets[pos], tablec->buckets[pos].capacity * 2);
+          __tablec_expandBucketArr(&tablec->buckets[hash], tablec->buckets[hash].capacity * 2);
 
-          tablec->buckets[pos].array[tablec->buckets[pos].capacity - 1].key = key;
-          tablec->buckets[pos].array[tablec->buckets[pos].capacity - 1].value = value;
-          tablec->buckets[pos].array[tablec->buckets[pos].capacity - 1].filled = 1;
+          tablec->buckets[hash].array[tablec->buckets[hash].capacity - 1].key = key;
+          tablec->buckets[hash].array[tablec->buckets[hash].capacity - 1].value = value;
 
-          tablec->buckets[pos].length++;
+          tablec->buckets[hash].length++;
 
           break;
-        } while (i < tablec->buckets[pos].length);
+        } while (i < tablec->buckets[hash].length);
 
         break;
       }
       case 1: {
-        if (!tablec->buckets[pos].emptySlots[0].filled) {
-          size_t i = -1;
+        if (tablec->buckets[hash].emptySlots[0].filled) {
+          tablec->buckets[hash].array[tablec->buckets[hash].emptySlots[0].index].key = key;
+          tablec->buckets[hash].array[tablec->buckets[hash].emptySlots[0].index].value = value;
 
-          do {
-            i++;
-            if (!tablec->buckets[pos].array[i].filled) {
-               tablec->buckets[pos].array[i].key = key;
-              tablec->buckets[pos].array[i].value = value;
-              tablec->buckets[pos].array[i].filled = 1;
-
-              tablec->buckets[pos].length++;
-              break;
-            }
-
-            if (i != tablec->buckets[pos].length - 1) continue;
-
-            __tablec_expandBucketArr(&tablec->buckets[pos], tablec->buckets[pos].capacity * 2);
-
-            tablec->buckets[pos].array[tablec->buckets[pos].capacity - 1].key = key;
-            tablec->buckets[pos].array[tablec->buckets[pos].capacity - 1].value = value;
-            tablec->buckets[pos].array[tablec->buckets[pos].capacity - 1].filled = 1;
-
-            tablec->buckets[pos].length++;
-
-            break;
-          } while (i < tablec->buckets[pos].length);
+          tablec->buckets[hash].emptySlots[0].index = 0;
+          tablec->buckets[hash].emptySlots[0].filled = 0;
 
           break;
         }
 
-        tablec->buckets[pos].array[tablec->buckets[pos].emptySlots[0].index].key = key;
-        tablec->buckets[pos].array[tablec->buckets[pos].emptySlots[0].index].value = value;
-        tablec->buckets[pos].array[tablec->buckets[pos].emptySlots[0].index].filled = 1;
+         __tablec_expandBucketArr(&tablec->buckets[hash], tablec->buckets[hash].capacity * 2);
 
-        tablec->buckets[pos].emptySlots[0].index = 0;
-        tablec->buckets[pos].emptySlots[0].filled = 0;
+        tablec->buckets[hash].array[tablec->buckets[hash].emptySlots[0].index].key = key;
+        tablec->buckets[hash].array[tablec->buckets[hash].emptySlots[0].index].value = value;
+
+        tablec->buckets[hash].emptySlots[0].index = 0;
+        tablec->buckets[hash].emptySlots[0].filled = 0;
 
         break;
       }
@@ -152,29 +129,27 @@ void tablec_set(struct hashtable *tablec, char *key, size_t keyLength, void *val
         size_t i = 0;
 
         do {
-          if (tablec->buckets[pos].emptySlots[i].filled) {
-            tablec->buckets[pos].array[tablec->buckets[pos].emptySlots[i].index].key = key;
-            tablec->buckets[pos].array[tablec->buckets[pos].emptySlots[i].index].value = value;
-            tablec->buckets[pos].array[tablec->buckets[pos].emptySlots[i].index].filled = 1;
+          if (tablec->buckets[hash].emptySlots[i].filled) {
+            tablec->buckets[hash].array[tablec->buckets[hash].emptySlots[i].index].key = key;
+            tablec->buckets[hash].array[tablec->buckets[hash].emptySlots[i].index].value = value;
 
-            tablec->buckets[pos].emptySlots[i].index = 0;
-            tablec->buckets[pos].emptySlots[i].filled = 0;
+            tablec->buckets[hash].emptySlots[i].index = 0;
+            tablec->buckets[hash].emptySlots[i].filled = 0;
 
-            tablec->buckets[pos].length++;
+            tablec->buckets[hash].length++;
             break;
           }
 
-          if (i == tablec->buckets[pos].emptyCapacity - 1) {
-            __tablec_expandBucketArr(&tablec->buckets[pos], tablec->buckets[pos].capacity * 2);
+          if (i == tablec->buckets[hash].emptyCapacity - 1) {
+            __tablec_expandBucketArr(&tablec->buckets[hash], tablec->buckets[hash].capacity * 2);
 
-            tablec->buckets[pos].array[tablec->buckets[pos].capacity - 1].key = key;
-            tablec->buckets[pos].array[tablec->buckets[pos].capacity - 1].value = value;
-            tablec->buckets[pos].array[tablec->buckets[pos].capacity - 1].filled = 1;
+            tablec->buckets[hash].array[tablec->buckets[hash].capacity - 1].key = key;
+            tablec->buckets[hash].array[tablec->buckets[hash].capacity - 1].value = value;
 
-            tablec->buckets[pos].length++;
+            tablec->buckets[hash].length++;
             break;
           }
-        } while (i < tablec->buckets[pos].emptyCapacity);
+        } while (i < tablec->buckets[hash].emptyCapacity);
 
         break;
       }
@@ -183,74 +158,72 @@ void tablec_set(struct hashtable *tablec, char *key, size_t keyLength, void *val
 }
 
 void tablec_del(struct hashtable *tablec, char *key, size_t keyLength) {
-  size_t pos = __tablec_mkIndex(tablec, key, keyLength);
+  size_t hash = __tablec_hash(tablec, key, keyLength);
 
   tablec->length--;
 
-  if (tablec->buckets[pos].capacity == 1) {
-    tablec->buckets[pos].array[0].key = NULL;
-    tablec->buckets[pos].array[0].value = NULL;
-    tablec->buckets[pos].array[0].filled = 0;
+  if (tablec->buckets[hash].capacity == 1) {
+    tablec->buckets[hash].array[0].key = NULL;
+    tablec->buckets[hash].array[0].value = NULL;
   } else {
     size_t i = -1;
     do {
       i++;
-      if (!tablec->buckets[pos].array[i].filled || strcmp(tablec->buckets[pos].array[i].key, key) != 0) continue;
+      if (tablec->buckets[hash].array[i].key == NULL || strcmp(tablec->buckets[hash].array[i].key, key) != 0) continue;
 
-      tablec->buckets[pos].array[i].key = NULL;
-      tablec->buckets[pos].array[i].value = NULL;
-      tablec->buckets[pos].array[i].filled = 0;
+      tablec->buckets[hash].array[i].key = NULL;
+      tablec->buckets[hash].array[i].value = NULL;
 
       break;
-    } while (i < tablec->buckets[pos].length);
+    } while (i < tablec->buckets[hash].length);
 
-    switch(tablec->buckets[pos].emptyCapacity) {
+    switch(tablec->buckets[hash].emptyCapacity) {
       case 0: {
-        tablec->buckets[pos].emptyCapacity++;
-        tablec->buckets[pos].emptySlots = malloc(sizeof(struct hashtable_buckets_array_empty));
+        tablec->buckets[hash].emptyCapacity++;
+        tablec->buckets[hash].emptySlots = malloc(sizeof(struct hashtable_buckets_array_empty));
 
-        tablec->buckets[pos].emptySlots[0].index = i;
-        tablec->buckets[pos].emptySlots[0].filled = 1;
+        tablec->buckets[hash].emptySlots[0].index = i;
+        tablec->buckets[hash].emptySlots[0].filled = 1;
 
         break;
       }
       case 1: {
-        if (tablec->buckets[pos].emptySlots[0].filled) {
-          tablec->buckets[pos].emptyCapacity++;
-          tablec->buckets[pos].emptySlots = malloc(sizeof(struct hashtable_buckets_array_empty) * tablec->buckets[pos].emptyCapacity);
+        if (tablec->buckets[hash].emptySlots[0].filled) {
+          tablec->buckets[hash].emptyCapacity++;
+          tablec->buckets[hash].emptySlots = malloc(sizeof(struct hashtable_buckets_array_empty) * tablec->buckets[hash].emptyCapacity);
 
-          tablec->buckets[pos].emptySlots[tablec->buckets[pos].emptyCapacity - 1].index = i;
-          tablec->buckets[pos].emptySlots[tablec->buckets[pos].emptyCapacity - 1].filled = 1;
+          tablec->buckets[hash].emptySlots[tablec->buckets[hash].emptyCapacity - 1].index = i;
+          tablec->buckets[hash].emptySlots[tablec->buckets[hash].emptyCapacity - 1].filled = 1;
 
           break;
         }
 
-        tablec->buckets[pos].emptySlots[0].index = i;
-        tablec->buckets[pos].emptySlots[0].filled = 1;
+        tablec->buckets[hash].emptySlots[0].index = i;
+        tablec->buckets[hash].emptySlots[0].filled = 1;
 
         break;
       }
       default: {
         size_t j = 2;
         do {
-          if (j >= tablec->buckets[pos].emptyCapacity - 1) {
-            tablec->buckets[pos].emptyCapacity++;
-            tablec->buckets[pos].emptySlots = realloc(tablec->buckets[pos].emptySlots, sizeof(struct hashtable_buckets_array_empty) * tablec->buckets[pos].emptyCapacity + 1);
+          if (j >= tablec->buckets[hash].emptyCapacity - 1) {
+            tablec->buckets[hash].emptyCapacity++;
+            tablec->buckets[hash].emptySlots = realloc(tablec->buckets[hash].emptySlots, sizeof(struct hashtable_buckets_array_empty) * tablec->buckets[hash].emptyCapacity + 1);
 
-            tablec->buckets[pos].emptySlots[j].index = i;
-            tablec->buckets[pos].emptySlots[j].filled = 1;
+            tablec->buckets[hash].emptySlots[j].index = i;
+            tablec->buckets[hash].emptySlots[j].filled = 1;
 
             break;
           }
 
-          if (!tablec->buckets[pos].emptySlots[j].filled) {
-            tablec->buckets[pos].emptySlots[j].index = i;
-            tablec->buckets[pos].emptySlots[j].filled = 1;
+          if (!tablec->buckets[hash].emptySlots[j].filled) {
+            tablec->buckets[hash].emptySlots[j].index = i;
+            tablec->buckets[hash].emptySlots[j].filled = 1;
 
             break;
           }
           j++;
-        } while (j < tablec->buckets[pos].emptyCapacity);
+        } while (j < tablec->buckets[hash].emptyCapacity);
 
         break;
       }
@@ -259,18 +232,18 @@ void tablec_del(struct hashtable *tablec, char *key, size_t keyLength) {
 }
 
 void *tablec_get(struct hashtable *tablec, char *key, size_t keyLength) {
-  size_t pos = __tablec_mkIndex(tablec, key, keyLength);
+  size_t hash = __tablec_hash(tablec, key, keyLength);
 
   size_t i = 0;
   void *value = NULL;
 
   do {
-    if (tablec->buckets[pos].array[i].filled && strcmp(tablec->buckets[pos].array[i].key, key) == 0) {
-      value = tablec->buckets[pos].array[i].value;
+    if (tablec->buckets[hash].array[i].key != NULL && strcmp(tablec->buckets[hash].array[i].key, key) == 0) {
+      value = tablec->buckets[hash].array[i].value;
       break;
     }
     i++;
-  } while (i < tablec->buckets[pos].length);
+  } while (i < tablec->buckets[hash].length);
 
   return value;
 }
