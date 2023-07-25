a9352e58c1971e1c1ecff41eb4c2aaa3d25aa5ef
ThePedroo <pedroolimpio152@gmail.com>
2023-07-25 14:33:57 -0300

    [IMPROVE] Return the bucket instead of only the value
    For more flexibility, TableC now returns the whole bucket. Also fixed warning in testing.

--- tablec.c
+++ tablec.c
@@ -12,7 +12,7 @@ void tablec_init(struct tablec_ht *tablec, struct tablec_bucket buckets[], size_
   memset(tablec->buckets, 0, sizeof(struct tablec_bucket) * capacity);
 }
 
-static size_t _tablec_hash_idx(struct tablec_ht *tablec, char *key) {
+static size_t _tablec_hash(struct tablec_ht *tablec, char *key) {
   size_t hash = 0;
   
   while (*key != '\0') hash = hash * 37 + (*(key++) & 255);
@@ -22,36 +22,31 @@ static size_t _tablec_hash_idx(struct tablec_ht *tablec, char *key) {
 
 int tablec_resize(struct tablec_ht *tablec, struct tablec_bucket new_buckets[], size_t new_capacity) {
   size_t old_capacity = tablec->capacity;
-  size_t idx;
-
-  if (new_capacity == old_capacity) {
-    fputs("Expect new bucket!\n", stderr);
-    abort();
-  }
+  size_t hash;
 
   tablec->capacity = new_capacity;
 
   memset(new_buckets, 0, sizeof(struct tablec_bucket) * new_capacity);
 
   while (old_capacity--) {
-
     if (tablec->buckets[old_capacity].key == NULL) continue;
 
-    idx = _tablec_hash_idx(tablec, tablec->buckets[old_capacity].key);
-    if (new_buckets[idx].key == NULL) {
-      new_buckets[idx].key = tablec->buckets[old_capacity].key;
-      new_buckets[idx].value = tablec->buckets[old_capacity].value;
+    hash = _tablec_hash(tablec, tablec->buckets[old_capacity].key);
+
+    if (new_buckets[hash].key == NULL) {
+      new_buckets[hash].key = tablec->buckets[old_capacity].key;
+      new_buckets[hash].value = tablec->buckets[old_capacity].value;
 
       continue;
     }
 
-    while ((new_buckets[idx].key != NULL) && (idx < tablec->capacity)) ++idx;
+    while ((new_buckets[hash].key != NULL) && (hash < tablec->capacity)) ++hash;
 
-    if (idx == tablec->capacity) return -1;
+    if (hash == tablec->capacity) return -1;
 
-    if (new_buckets[idx].key == NULL) {
-      new_buckets[idx].key = tablec->buckets[old_capacity].key;
-      new_buckets[idx].value = tablec->buckets[old_capacity].value;
+    if (new_buckets[hash].key == NULL) {
+      new_buckets[hash].key = tablec->buckets[old_capacity].key;
+      new_buckets[hash].value = tablec->buckets[old_capacity].value;
     }
   }
 
@@ -61,50 +56,57 @@ int tablec_resize(struct tablec_ht *tablec, struct tablec_bucket new_buckets[], 
 }
 
 int tablec_set(struct tablec_ht *tablec, char *key, void *value) {
-  size_t idx;
+  size_t hash;
 
   if ((tablec->length - 1) == tablec->capacity) return -1;
 
-  idx = _tablec_hash_idx(tablec, key);
-  while (idx != tablec->capacity) {
-    if (tablec->buckets[idx].key == NULL) {
-      tablec->buckets[idx].key = key;
-      tablec->buckets[idx].value = value;
+  hash = _tablec_hash(tablec, key);
+
+  while (hash != tablec->capacity) {
+    if (tablec->buckets[hash].key == NULL) {
+      tablec->buckets[hash].key = key;
+      tablec->buckets[hash].value = value;
 
       ++tablec->length;
 
       return tablec->capacity - tablec->length + 1;
     }
 
-    ++idx;
+    ++hash;
   }
 
   return -1;
 }
 
 void tablec_del(struct tablec_ht *tablec, char *key) {
-  size_t idx = _tablec_hash_idx(tablec, key);
-  while (idx != tablec->capacity) {
-    if (tablec->buckets[idx].key != NULL && strcmp(tablec->buckets[idx].key, key) == 0) {
-      tablec->buckets[idx].key = NULL;
-      tablec->buckets[idx].value = NULL;
+  size_t hash = _tablec_hash(tablec, key);
+
+  while (hash != tablec->capacity) {
+    if (tablec->buckets[hash].key != NULL && strcmp(tablec->buckets[hash].key, key) == 0) {
+      tablec->buckets[hash].key = NULL;
+      tablec->buckets[hash].value = NULL;
       tablec->length--;
 
       return;
     }
 
-    ++idx;
+    ++hash;
   }
 }
 
-void *tablec_get(struct tablec_ht *tablec, char *key) {
-  size_t idx = _tablec_hash_idx(tablec, key);
-  while (idx != tablec->capacity) {
-    if (tablec->buckets[idx].key != NULL && strcmp(tablec->buckets[idx].key, key) == 0)
-      return tablec->buckets[idx].value;
+struct tablec_bucket tablec_get(struct tablec_ht *tablec, char *key) {
+  struct tablec_bucket empty;
+  size_t hash = _tablec_hash(tablec, key);
 
-    ++idx;
+  while (hash != tablec->capacity) {
+    if (tablec->buckets[hash].key != NULL && strcmp(tablec->buckets[hash].key, key) == 0)
+      return tablec->buckets[hash];
+
+    ++hash;
   }
 
-  return NULL;
+  empty.key = NULL;
+  empty.value = NULL;
+
+  return empty;
 }
--- tests/testing.c
+++ tests/testing.c
@@ -33,7 +33,7 @@ TEST setting_key(void) {
   tablec_init(&tablec, buckets, 16);
 
   tablec_set(&tablec, key, value);
-  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, key), value), 0);
+  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, key).value, value), 0);
 
   PASSm("TableC was able to set a key and a value.");
 }
@@ -44,13 +44,13 @@ TEST setting_key_and_read_after_deleting(void) {
   tablec_init(&tablec, buckets, 16);
 
   tablec_set(&tablec, key, value);
-  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, key), value), 0);
+  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, key).value, value), 0);
 
   tablec_del(&tablec, key);
-  ASSERT_EQm("TableC was not able to delete key.", tablec_get(&tablec, key) == NULL ? 0 : 1, 0);
+  ASSERT_EQm("TableC was not able to delete key.", tablec_get(&tablec, key).value == NULL ? 0 : 1, 0);
 
   tablec_set(&tablec, key, value);
-  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, key), value), 0);
+  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, key).value, value), 0);
 
   PASSm("TableC was able to set a key and a value after deleting the same key.");
 }
@@ -61,16 +61,16 @@ TEST setting_2_keys_with_the_same_pos_and_deleting(void) {
   tablec_init(&tablec, buckets, 100);
 
   tablec_set(&tablec, key, value);
-  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, key), value), 0);
+  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, key).value, value), 0);
 
   tablec_set(&tablec, otherKey, value);
-  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, otherKey), value), 0);
+  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, otherKey).value, value), 0);
 
   tablec_del(&tablec, key);
-  ASSERT_EQm("TableC was not able to delete key.", tablec_get(&tablec, key) == NULL ? 0 : 1, 0);
+  ASSERT_EQm("TableC was not able to delete key.", tablec_get(&tablec, key).value == NULL ? 0 : 1, 0);
 
   tablec_set(&tablec, key, value);
-  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, key), value), 0);
+  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, key).value, value), 0);
 
   PASSm("TableC was able to set a key and a value after deleting the same key.");
 }
@@ -81,7 +81,7 @@ TEST deleting_and_reading_non_exist_key(void) {
   tablec_init(&tablec, buckets, 16);
 
   tablec_del(&tablec, key);
-  ASSERT_EQm("TableC was not able to delete key.", tablec_get(&tablec, key) == NULL ? 0 : 1, 0);
+  ASSERT_EQm("TableC was not able to delete key.", tablec_get(&tablec, key).value == NULL ? 0 : 1, 0);
 
   PASSm("TableC was able to delete a non existent key and not segfault.");
 }
@@ -92,13 +92,13 @@ TEST see_empty_slots_after_deleting(void) {
   tablec_init(&tablec, buckets, 100);
 
   tablec_set(&tablec, key, value);
-  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, key), value), 0);
+  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, key).value, value), 0);
 
   tablec_set(&tablec, otherKey, value);
-  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, otherKey), value), 0);
+  ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, otherKey).value, value), 0);
 
   tablec_del(&tablec, key);
-  ASSERT_EQm("TableC was not able to delete key.", tablec_get(&tablec, key) == NULL, 1);
+  ASSERT_EQm("TableC was not able to delete key.", tablec_get(&tablec, key).value == NULL, 1);
 
   PASSm("TableC was able to add the empty slot index to the empty slots array (successful deletion).");
 }
@@ -107,7 +107,7 @@ TEST test_resize(void) {
   char keyRandom[1000];
   size_t i = 0;
   struct tablec_bucket buckets[100];
-  struct tablec_ht buckets2[1000];
+  struct tablec_bucket buckets2[1000];
 
   struct tablec_ht tablec;
   tablec_init(&tablec, buckets, 100);
@@ -116,7 +116,7 @@ TEST test_resize(void) {
     sprintf(keyRandom, "%zu", i);
     tablec_resize(&tablec, buckets2, tablec.capacity + 1);
     tablec_set(&tablec, keyRandom, value);
-    ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, keyRandom), value), 0);
+    ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, keyRandom).value, value), 0);
   }
 
   PASSm("TableC was able to resize the hashtable.");
@@ -133,7 +133,7 @@ TEST mini_fuzz_testing(void) {
   while(i++ < 1000) {
     sprintf(keyRandom, "%d", rand() % 1000);
     tablec_set(&tablec, keyRandom, value);
-    ASSERT_EQm("TableC was not able to set a key and a value.", strcmp(tablec_get(&tablec, keyRandom), value), 0);
+    ASSERT_EQm("TableC was not able to set a key and a value.", strcmp((char *)tablec_get(&tablec, keyRandom).value, value), 0);
   }
 
   PASSm("TableC was able to pass the mini fuzzy testing.");
